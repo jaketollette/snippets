2583758f70f0f88567074b0c132d842d47b86da4
jaketollette <jake@jaketollette.com>
2016-08-20 00:16:02 -0500

    Integrate bourbon neat

---
+++ js/general.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    "use strict"
+    $("#HeroSlider, #Section_2_Slider, #Section_4_Slider").slick({
+        dots: true,
+        mobileFirst: true,
+        arrows: true
+    });
+
+});
