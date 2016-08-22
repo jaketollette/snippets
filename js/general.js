3f39de1a168f5f71eb07d3f76a19289b1a54cc47
jaketollette <jake@jaketollette.com>
2016-08-22 01:23:40 -0500

    Responsive nav

--- js/general.js
+++ js/general.js
@@ -1,9 +1,33 @@
 $(document).ready(function () {
     "use strict"
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+            $("#uni-header").addClass("fixed");
+        } else {
+            $("#uni-header").removeClass("fixed");
+        }
+    });
     $("#HeroSlider, #Section_2_Slider, #Section_4_Slider").slick({
         dots: true,
         mobileFirst: true,
         arrows: true
     });
-
+    $('#Section_1 div:nth-child(1), #Section_1 div:nth-child(2)').matchHeight({
+        property: 'min-height'
+    });
+    // $('.btn-list a.dropdown').hover(function (e) {
+    //    var width = $(this).outerWidth();
+    //     $(this).next('div.sub-menu').css('width', width).removeClass('hide');
+    // }, function() {
+    //   $(this).next('div.sub-menu').addClass('hide');
+    // });
+    $('.btn-list a.dropdown').on('mouseenter click focus', function(e){
+      e.preventDefault();
+      var width = $(this).outerWidth();
+      $(this).next('div.sub-menu').css('width', width).removeClass('hide');
+    });
+    $('.btn-list a.dropdown').on('mouseleave click blur', function(e){
+      e.preventDefault();
+      $(this).next('div.sub-menu').addClass('hide');
+    });
 });
